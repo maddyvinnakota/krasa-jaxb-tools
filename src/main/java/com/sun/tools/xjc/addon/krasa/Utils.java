db25dac9fd4c28dd6bf5736ab7afbac3137125ef
Francesco Illuminati <fillumina@gmail.com>
2022-09-04 22:03:37 +0200

    refactored Utils methods, tests added

--- src/main/java/com/sun/tools/xjc/addon/krasa/Utils.java
+++ src/main/java/com/sun/tools/xjc/addon/krasa/Utils.java
@@ -2,112 +2,166 @@ package com.sun.tools.xjc.addon.krasa;
 
 import com.sun.codemodel.JFieldVar;
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Vojtech Krasa
  */
 class Utils {
 
-    public static final String[] NUMBERS = new String[] {
-        "BigDecimal", "BigInteger", "String", "byte", "short", "int", "long"
-    };
-
     public static int toInt(Object value) {
-        if (value instanceof BigInteger) {
-			// xjc
-            return ((BigInteger) value).intValue();
-        } else if (value instanceof Integer) {
-			// cxf-codegen
-            return (Integer) value;
-		} else {
-            throw new IllegalArgumentException(
-                    "unknown type " + value.getClass());
-		}
-	}
+        Objects.requireNonNull(value, "value is null");
+        if (value instanceof Number) {
+            return ((Number) value).intValue();
+        } else {
+            throw new IllegalArgumentException("unknown type " + value.getClass());
+        }
+    }
 
-	private static Field getSimpleField(String fieldName, Class<?> clazz) {
-		Class<?> tmpClass = clazz;
-		try {
-			do {
-				for (Field field : tmpClass.getDeclaredFields()) {
-					String candidateName = field.getName();
-					if (!candidateName.equals(fieldName)) {
-						continue;
-					}
-					field.setAccessible(true);
-					return field;
-				}
-				tmpClass = tmpClass.getSuperclass();
-			} while (tmpClass != null);
-		} catch (Exception e) {
-			System.err.println("krasa-jaxb-tools - Field '" + fieldName +
-                    "' not found on class " + clazz);
-		}
-		return null;
-	}
+    /**
+     * Return the value of a field (even private ones) using reflection.
+     */
+    static Field getSimpleField(String fieldName, Class<?> clazz) {
+        Class<?> currentClass = clazz;
+        try {
+            do {
+                try {
+                    Field field = currentClass.getDeclaredField(fieldName);
+                    field.setAccessible(true);
+                    return field;
+                } catch (NoSuchFieldException ex) {
+                    currentClass = currentClass.getSuperclass();
+                }
+            } while (currentClass != Class.class);
+        } catch (Exception e) {
+            System.err.println("krasa-jaxb-tools - Field '" + fieldName +
+                    "' not found in class " + clazz);
+        }
+        return null;
+    }
 
-    // TODO this
-    public static Object getField(String path, Object obj) {
-		try {
+    /**
+     * Return the value of a field (even private ones) using reflection.
+     *
+     * @param path sequence of 1 or more field name separated by '.'. i.e.: 'office.address.street'
+     * @param obj the object to evaluate
+     * @return the value of the specified field
+     */
+    public static Object _getFieldValue(String path, Object obj) {
+        try {
             int idx = path.indexOf(".");
             if (idx != -1) {
                 String field = path.substring(0, idx);
                 Field declaredField = obj.getClass().getDeclaredField(field);
-				declaredField.setAccessible(true);
+                declaredField.setAccessible(true);
                 Object result = declaredField.get(obj);
 
-                String nextField = path.substring(path.indexOf(".") + 1);
-                return getField(nextField, result);
-			} else {
+                String nextField = path.substring(idx + 1);
+                return getFieldValue(nextField, result);
+            } else {
                 Field simpleField = getSimpleField(path, obj.getClass());
-				simpleField.setAccessible(true);
+                simpleField.setAccessible(true);
                 return simpleField.get(obj);
-			}
-		} catch (Exception e) {
-			String className = null;
-			if(obj != null && obj.getClass() != null) {
-				className = obj.getClass().getName();
-			}
-			System.err.println("krasa-jaxb-tools - Field " + path + " not found on " + className );
-		}
-		return null;
-	}
+            }
+        } catch (Exception e) {
+            String className = null;
+            if (obj != null && obj.getClass() != null) {
+                className = obj.getClass().getName();
+            }
+            System.err.println("krasa-jaxb-tools - Field " + path + " not found on " + className);
+        }
+        return null;
+    }
 
-	public static boolean isMin(String value) {
-        return "-9223372036854775808".equals(value) || "-2147483648".equals(value);
-	}
+    /**
+     * Return the value of a field (even private ones) using reflection.
+     *
+     * @param path sequence of 1 or more field name separated by '.'. i.e.: 'office.address.street'
+     * @param obj the object to evaluate
+     * @return the value of the specified field
+     */
+    public static Object getFieldValue(String path, Object obj) {
+        String[] fieldNames = path.split("\\.");
+        Object actualObject = obj;
+        try {
+            for (String fieldName : fieldNames) {
+                Objects.requireNonNull(actualObject, "null object on field '" + fieldName +
+                        "' of " + obj.getClass().getCanonicalName() + "->" + path);
+                Field field = getSimpleField(fieldName, actualObject.getClass());
+                if (field == null) {
+                    throw new IllegalArgumentException("cannot find field '" + fieldName + "' in " +
+                            obj.getClass().getCanonicalName() + "->" + path);
+                }
+                field.setAccessible(true);
+                actualObject = field.get(actualObject);
+            }
+            return actualObject;
+        } catch (IllegalAccessException e) {
+            throw new AssertionError(e);
+        }
+    }
 
-	public static boolean isMax(String value) {
-        return "9223372036854775807".equals(value) || "2147483647".equals(value);
-	}
+    private static final String LONG_MIN = Long.toString(Long.MIN_VALUE);
+    private static final String LONG_MAX = Long.toString(Long.MAX_VALUE);
+    private static final String INTEGER_MIN = Integer.toString(Integer.MIN_VALUE);
+    private static final String INTEGER_MAX = Integer.toString(Integer.MAX_VALUE);
 
-	public static boolean isNumber(JFieldVar field) {
-        final String fieldTypeName = field.type().name();
-		for (String type : NUMBERS) {
-            if (type.equalsIgnoreCase(fieldTypeName)) {
-				return true;
-			}
-		}
-		try {
-            final String fieldTypeFullName = field.type().fullName();
+    public static boolean isMin(String value) {
+        return LONG_MIN.equals(value) || INTEGER_MIN.equals(value);
+    }
+
+    public static boolean isMax(String value) {
+        return LONG_MAX.equals(value) || INTEGER_MAX.equals(value);
+    }
+
+    public static final Set<String> NUMBERS = Arrays.stream( new Class<?>[] {
+            BigDecimal.class,
+            BigInteger.class,
+            Byte.class,
+            Short.class,
+            Integer.class,
+            Double.class,
+            Float.class,
+            Long.class})
+                .map(c -> c.getSimpleName().toUpperCase())
+                .collect(Collectors.toSet());
+
+    public static boolean isNumberField(JFieldVar field) {
+        return isFieldTypeNameNumber(field.type().name()) ||
+                isFieldTypeFullNameNumber(field.type().fullName());
+    }
+
+    static boolean isFieldTypeNameNumber(String fieldTypeName) {
+        if (NUMBERS.contains(fieldTypeName.toUpperCase())) {
+            return true;
+        }
+        return false;
+    }
+
+    static boolean isFieldTypeFullNameNumber(String fieldTypeFullName) {
+        try {
             if (isNumber(Class.forName(fieldTypeFullName))) {
                 return true;
             }
-		} catch (Exception e) {
-			// whatever
-		}
-		return false;
-	}
+        } catch (ClassNotFoundException ex) {
+            // ignore
+        }
+        return false;
+    }
 
-	public static boolean isNumber(Class<?> aClass) {
-		return Number.class.isAssignableFrom(aClass);
-	}
+    static boolean isNumber(Class<?> aClass) {
+        return Number.class.isAssignableFrom(aClass);
+    }
 
-	public static boolean isCustomType(JFieldVar var) {
-		if(var == null) {
-			return false;
-		}
+    public static boolean isCustomType(JFieldVar var) {
+        if (var == null) {
+            return false;
+        }
         return "JDirectClass".equals(var.type().getClass().getSimpleName());
     }
 }
--- src/test/java/com/sun/tools/xjc/addon/krasa/UtilsTest.java
+++ src/test/java/com/sun/tools/xjc/addon/krasa/UtilsTest.java
@@ -1,6 +1,11 @@
 package com.sun.tools.xjc.addon.krasa;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,5 +19,136 @@ public class UtilsTest {
         Assert.assertFalse(Utils.isNumber(String.class));
         Assert.assertFalse(Utils.isNumber(IllegalStateException.class));
         Assert.assertTrue(Utils.isNumber(BigDecimal.class));
+    }
+
+    @Test
+    public void integerToIntTest() {
+        assertEquals(12, Utils.toInt(Integer.valueOf(12)) );
+        assertEquals(-3, Utils.toInt(Integer.valueOf(-3)) );
+    }
+
+    @Test
+    public void bigIntegerToIntTest() {
+        assertEquals(12, Utils.toInt(BigInteger.valueOf(12)) );
+        assertEquals(-3, Utils.toInt(BigInteger.valueOf(-3)) );
+    }
+
+    @Test
+    public void longToIntTest() {
+        Utils.toInt(Long.valueOf(7L));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotAcceptStringToIntTest() {
+        Utils.toInt("7");
+    }
+
+    public static class A {
+        public static float PUBLIC_VALUE = 1.23f;
+        public static String PRIVATE_VALUE = "private";
+
+        public float aPublic = PUBLIC_VALUE;
+        private String aPrivate = PRIVATE_VALUE;
+    }
+
+    public static class B extends A {
+    }
+
+    public static class C {
+        B b = new B();
+    }
+
+    public static class D {
+        C c = new C();
+    }
+
+    @Test
+    public void shouldGetAPrivateFieldFromClass() {
+        assertNotNull(Utils.getSimpleField("aPrivate", A.class));
+    }
+
+    @Test
+    public void shouldGetAPublicFieldFromClass() {
+        assertNotNull(Utils.getSimpleField("aPublic", A.class));
+    }
+
+    @Test
+    public void shouldGetAPrivateFieldFromSuperClass() {
+        assertNotNull(Utils.getSimpleField("aPrivate", B.class));
+    }
+
+    @Test
+    public void shouldGetAPublicFieldFromSuperClass() {
+        assertNotNull(Utils.getSimpleField("aPublic", B.class));
+    }
+
+    @Test
+    public void shoudGetFieldValue() {
+        A a = new A();
+        B b = new B();
+
+        assertEquals(A.PRIVATE_VALUE, Utils.getFieldValue("aPrivate", a));
+        assertEquals(A.PUBLIC_VALUE, Utils.getFieldValue("aPublic", a));
+        assertEquals(A.PRIVATE_VALUE, Utils.getFieldValue("aPrivate", b));
+        assertEquals(A.PUBLIC_VALUE, Utils.getFieldValue("aPublic", b));
+    }
+
+    @Test
+    public void shouldGetCompositePathFieldValue() {
+        C c = new C();
+        assertEquals(A.PRIVATE_VALUE, Utils.getFieldValue("b.aPrivate", c));
+        assertEquals(A.PUBLIC_VALUE, Utils.getFieldValue("b.aPublic", c));
+
+        D d = new D();
+        assertEquals(A.PRIVATE_VALUE, Utils.getFieldValue("c.b.aPrivate", d));
+        assertEquals(A.PUBLIC_VALUE, Utils.getFieldValue("c.b.aPublic", d));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotFindAnInexistentField() {
+        Utils.getFieldValue("inexistent", new B());
+    }
+
+    @Test
+    public void shouldTestIsMin() {
+        assertTrue(Utils.isMin("-9223372036854775808"));
+        assertTrue(Utils.isMin("-2147483648"));
+        assertFalse(Utils.isMin("-2147483649"));
+    }
+
+    @Test
+    public void shouldTestIsMax() {
+        assertTrue(Utils.isMax("9223372036854775807"));
+        assertTrue(Utils.isMax("2147483647"));
+        assertFalse(Utils.isMax("2147483648"));
+    }
+
+    public static class TypeTest {
+        private String stringField;
+        private int intField;
+        private Long longField;
+        private BigInteger bigIntegerField;
+    }
+
+    @Test
+    public void isFieldTypeNameNumber() {
+        assertTrue(Utils.isFieldTypeNameNumber(BigDecimal.class.getSimpleName()));
+        assertTrue(Utils.isFieldTypeNameNumber(Long.class.getSimpleName()));
+        assertTrue(Utils.isFieldTypeNameNumber(Short.class.getSimpleName()));
+        assertTrue(Utils.isFieldTypeNameNumber(Integer.class.getSimpleName()));
+
+        assertFalse(Utils.isFieldTypeNameNumber(String.class.getSimpleName()));
+        assertFalse(Utils.isFieldTypeNameNumber(Boolean.class.getSimpleName()));
+    }
+
+    @Test
+    public void isFieldTypeFullNameNumber() {
+        assertTrue(Utils.isFieldTypeFullNameNumber(BigDecimal.class.getCanonicalName()));
+        assertTrue(Utils.isFieldTypeFullNameNumber(Long.class.getCanonicalName()));
+        assertTrue(Utils.isFieldTypeFullNameNumber(Short.class.getCanonicalName()));
+        assertTrue(Utils.isFieldTypeFullNameNumber(Integer.class.getCanonicalName()));
+
+        assertFalse(Utils.isFieldTypeFullNameNumber(String.class.getCanonicalName()));
+        assertFalse(Utils.isFieldTypeFullNameNumber(Boolean.class.getCanonicalName()));
     }
 }
