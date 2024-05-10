a0a84ea568a53a67fba88e735b4867e5e74569c2
Francesco Illuminati <fillumina@gmail.com>
2024-05-10 19:50:35 +0200

    optimize log prefix string

--- src/main/java/com/sun/tools/xjc/addon/krasa/validations/ValidationsLogger.java
+++ src/main/java/com/sun/tools/xjc/addon/krasa/validations/ValidationsLogger.java
@@ -8,11 +8,12 @@ import java.util.Map;
  * @author Francesco Illuminati <fillumina@gmail.com>
  */
 interface ValidationsLogger {
+    static final String PREFIX = "[" + JaxbValidationsPlugin.class.getSimpleName() + "] ";
 
     void addAnnotation(String annotationName, Map<String, String> parameterMap);
 
     public static void log(String message) {
-        System.out.println("[" + JaxbValidationsPlugin.class.getSimpleName() + "] " + message);
+        System.out.println(PREFIX + message);
     }
 
 }
