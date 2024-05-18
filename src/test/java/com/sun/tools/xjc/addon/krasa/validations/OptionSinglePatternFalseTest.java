b60b2a19697b488323a3b6925e6f209a8a90abc8
Francesco Illuminati <fillumina@gmail.com>
2024-05-18 13:16:19 +0200

    add test for singlePattern argument

---
+++ src/test/java/com/sun/tools/xjc/addon/krasa/validations/OptionSinglePatternFalseTest.java
@@ -0,0 +1,26 @@
+package com.sun.tools.xjc.addon.krasa.validations;
+
+import java.util.List;
+
+/**
+ * Test default parameters
+ *
+ * @author Francesco Illuminati <fillumina@gmail.com>
+ */
+public class OptionSinglePatternFalseTest extends OptionBase {
+
+    @Override
+    public String getAnnotationFileName() {
+        return "singlePattern-false-annotation.txt";
+    }
+
+
+    @Override
+    public List<String> getArgs() {
+        // overwrite whatever options set by RunXJC2MojoTestHelper so to keep default configuration
+        return ArgumentBuilder.builder()
+                .add(ValidationsArgument.singlePattern, false)
+                .getOptionList();
+    }
+
+}
