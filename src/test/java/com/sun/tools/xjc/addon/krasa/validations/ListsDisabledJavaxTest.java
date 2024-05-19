8e0e9f7bc0eec48dd47667a1fb9e6dfb49e81567
Francesco Illuminati <fillumina@gmail.com>
2024-05-19 11:52:42 +0200

    add test for generateStringListAnnotations=false

---
+++ src/test/java/com/sun/tools/xjc/addon/krasa/validations/ListsDisabledJavaxTest.java
@@ -0,0 +1,24 @@
+package com.sun.tools.xjc.addon.krasa.validations;
+
+import java.util.List;
+
+public class ListsDisabledJavaxTest extends AnnotationsMojoTestHelper {
+
+    public ListsDisabledJavaxTest() {
+        super("lists", ValidationsAnnotation.JAVAX);
+    }
+
+    @Override
+    public String getAnnotationFileName() {
+        return "disabled-list-annotation.txt";
+    }
+
+    @Override
+    public List<String> getArgs() {
+        return ArgumentBuilder.builder()
+                .add(ValidationsArgument.generateStringListAnnotations, false)
+                .add(ValidationsArgument.targetNamespace, getNamespace())
+                .add(ValidationsArgument.validationAnnotations, getAnnotation().name())
+                .getOptionList();
+    }
+}
